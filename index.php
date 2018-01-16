9efe372d200e77dcc911eb14e52d876031e01950
vchandwani <varunchandwani@gmail.com>
2018-01-16 14:11:04 +1100

    New Images added for Theme. Scroll top added

--- index.php
+++ index.php
@@ -70,9 +70,24 @@
                         $(this).removeClass('display-none');
                     }
                 });
-            });
-                       
-        });      
+            });                    
+        }); 
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("myBtn").style.display = "block";
+            } else {
+                document.getElementById("myBtn").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }      
     </script>
     
 </head>
@@ -219,7 +234,7 @@
     }
     $_POST = null;
     ?>
-    <div class="container-fluid">
+    <div class="container-fluid background-animation">
         <div class="row">
             <nav class="col-sm-3" id="myScrollspy">
                 <ul class="nav nav-pills nav-stacked">
